c1b5bf99af9419d39206b9f9e82ab0e1800e71c0
DMGUK <dmytro.guk@gmail.com>
2025-10-18 13:52:00 +0200

    Junior Frontend Angular Developer Task

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localePl);
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    ...appConfig.providers,
+    { provide: 'LOCALE_ID', useValue: 'pl' } // optional if already in component
+  ]
+}).catch(err => console.error(err));
